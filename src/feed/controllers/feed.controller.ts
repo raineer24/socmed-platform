ceb56d3d71724121121b1465c81ba651cad2921d
wtf <mura man kag wtf>
2022-07-02 12:04:52 +0800

    api socmed nestjs integration

---
+++ src/feed/controllers/feed.controller.ts
@@ -0,0 +1,41 @@
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Put,
+  Delete,
+} from '@nestjs/common';
+import { FeedPost } from '../models/post.interface';
+import { FeedService } from '../services/feed.service';
+import { Observable } from 'rxjs';
+import { DeleteResult, UpdateResult } from 'typeorm';
+
+@Controller('feed')
+export class FeedController {
+  constructor(private feedService: FeedService) {}
+
+  @Post()
+  create(@Body() post: FeedPost): Observable<FeedPost> {
+    return this.feedService.createPost(post);
+  }
+
+  @Get()
+  findAll(): Observable<FeedPost[]> {
+    return this.feedService.findAllPosts();
+  }
+
+  @Put(':id')
+  update(
+    @Param('id') id: number,
+    @Body() feedPost: FeedPost,
+  ): Observable<UpdateResult> {
+    return this.feedService.updatePost(id, feedPost);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: number): Observable<DeleteResult> {
+    return this.feedService.deletePost(id);
+  }
+}
